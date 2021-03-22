b4e406b36e800090cc1d70e68c1721255dcd674c
nitro <nitromacchitato@users.noreply.github.com>
2021-03-22 12:27:25 -0400

    added a freature to check if the user exist before uploading their data to the database

--- like-me-app/pages/api/insert.js
+++ like-me-app/pages/api/insert.js
@@ -14,8 +14,14 @@ export default async function handler(req,res){
     const code = req.body['code']
     const key = req.body['secret']
 
-  
+
+
     if(key === process.env.LIKE_ME_API_KEY ){
+        //STATUS LOG - SUCCESSFULY CONNECTED TO API 
+        console.log('Connected to API')
+
+        // Create a connection to mongodb 
+        const {db} = await connectToDatabase();
 
         // Generate a new access and refresh token using our code given during authroization 
         const getToken = await getRefreshToken(code)
@@ -30,99 +36,111 @@ export default async function handler(req,res){
 
 
         // Detailed User Info 
-        const display_name = user['display_name']
-        //const external_urls = user['external_urls']['spotify']
-        let image = user['images']
-        if(image === undefined){
-            image = "../../public/img/user.png"
-        }
-        const profilePage = `https://open.spotify.com/user/${display_name}`
+        const display_name = await user['display_name']
+      
+        // See if the user already exist in the database 
+        const exist = await db.collection('users').find({'user': display_name}).count() > 0;
+        
+        console.log('Exist is',exist)
 
+        if(exist != true){
 
-        // STATUS LOG - USER INFORMATION 
-        console.log('Generated User Info')
-
-
-        // Get the users saved tracks and save them into an array 
-        const tracks = []
-
-        let SAVED_TRACKS_ENDPOINT = `https://api.spotify.com/v1/me/tracks`;
-        const getTracks = await getSavedTracks(accessToken,SAVED_TRACKS_ENDPOINT)
-        const trackItems = await getTracks.json()
-
-        const totalTracks = await trackItems['total']
-        const nextTrackSet = await trackItems['next'] // url to next 20 songs || will be null if list is empty 
-        const listofTracks = await trackItems['items'] // list of songs 
-
-
-        // Adds each song id to the list of tracks 
-        listofTracks.map((song) => {
-
-            // Get the song ID 
-            const songURI = song['track']['uri'].split(':')[2]
-
-            // Add the songURI to the array if it's not already in it 
-            if(tracks.includes(songURI) == false){
-                tracks.push(songURI)
+            // User Profile Picture 
+            let image = user['images']
+            if(image === undefined){
+                image = "../../public/img/user.png"
             }
-            
-        })
+            const profilePage = `https://open.spotify.com/user/${display_name}`
 
 
-        // Set variables to track links 
-        let links = []
-        let count = 50; 
-
-        // Generates all the links to get the first 10,0000 saved songs from the user or their total tracks 
-        // Whichever comes first 
-        while (count <= totalTracks ){
-
-            let nextLink = `https://api.spotify.com/v1/me/tracks?limit=50&offset=${count}`;
-            links.push(nextLink);
-            count = count + 50;
-
-        }
+            // STATUS LOG - USER INFORMATION 
+            console.log('Generated User Info')
 
 
-        // Make a request to every link to get all the sets of songs and save them onto the array of tracks 
-        let requestCount = 0; 
-        while (requestCount < links.length){
+            // Get the users saved tracks and save them into an array 
+            const tracks = []
 
-            let SAVED_TRACKS_ENDPOINT = links[requestCount];
+            let SAVED_TRACKS_ENDPOINT = `https://api.spotify.com/v1/me/tracks`;
             const getTracks = await getSavedTracks(accessToken,SAVED_TRACKS_ENDPOINT)
             const trackItems = await getTracks.json()
-        
+
             const totalTracks = await trackItems['total']
             const nextTrackSet = await trackItems['next'] // url to next 20 songs || will be null if list is empty 
             const listofTracks = await trackItems['items'] // list of songs 
-        
+
+
             // Adds each song id to the list of tracks 
             listofTracks.map((song) => {
-        
+
                 // Get the song ID 
                 const songURI = song['track']['uri'].split(':')[2]
-        
+
                 // Add the songURI to the array if it's not already in it 
                 if(tracks.includes(songURI) == false){
                     tracks.push(songURI)
                 }
                 
             })
-            requestCount = requestCount + 1 
+
+
+            // Set variables to track links 
+            let links = []
+            let count = 50; 
+
+            // Generates all the links to get the first 10,0000 saved songs from the user or their total tracks 
+            // Whichever comes first 
+            while (count <= totalTracks ){
+
+                let nextLink = `https://api.spotify.com/v1/me/tracks?limit=50&offset=${count}`;
+                links.push(nextLink);
+                count = count + 50;
+
+            }
+
+
+            // Make a request to every link to get all the sets of songs and save them onto the array of tracks 
+            let requestCount = 0; 
+            while (requestCount < links.length){
+
+                let SAVED_TRACKS_ENDPOINT = links[requestCount];
+                const getTracks = await getSavedTracks(accessToken,SAVED_TRACKS_ENDPOINT)
+                const trackItems = await getTracks.json()
+            
+                const totalTracks = await trackItems['total']
+                const nextTrackSet = await trackItems['next'] // url to next 20 songs || will be null if list is empty 
+                const listofTracks = await trackItems['items'] // list of songs 
+            
+                // Adds each song id to the list of tracks 
+                listofTracks.map((song) => {
+            
+                    // Get the song ID 
+                    const songURI = song['track']['uri'].split(':')[2]
+            
+                    // Add the songURI to the array if it's not already in it 
+                    if(tracks.includes(songURI) == false){
+                        tracks.push(songURI)
+                    }
+                    
+                })
+                requestCount = requestCount + 1 
+            }
+
+            // STATUS LOG - USER INFORMATION 
+            console.log('Songs Fully Uploaded')
+        
+        
+            // Connect to listings and review
+            const data = await db.collection('users').insertOne({user:display_name,profile:profilePage,image:image,songs:tracks});
+
+            // STATUS LOG - DATABASE UPDATE 
+            console.log('User Uploaded To Database')
+
+            res.json(data);
+
+        } else {
+            console.log('User Already Exist')
+            res.json({exist:true})
         }
-
-        // STATUS LOG - USER INFORMATION 
-        console.log('Songs Fully Uploaded')
-    
-        // Create a connection to mongodb 
-        const {db} = await connectToDatabase();
-        // Connect to listings and review
-        const data = await db.collection('users').insertOne({user:display_name,profile:profilePage,image:image,songs:tracks});
-
-        // STATUS LOG - DATABASE UPDATE 
-        console.log('User Uploaded To Database')
-
-        res.json(data);
 
     } else {
 
--- like-me-app/pages/index.js
+++ like-me-app/pages/index.js
@@ -16,97 +16,75 @@ export default function Home({SPOTIFY_AUTH_URL}) {
       <link href="/fonts/style.css" rel="stylesheet"/>
     </Head>
 
-    <body>
+    <section className="hero is-fullheight">
 
-      <section className="hero is-fullheight">
+      {/* HEADER */}
+      <div className="hero-head" style={{margin:"0 auto"}}>
+        <div className="columns is-mobile is-marginless heading has-text-weight-bold">
+          <div className="column center">
+            <p className="navbar-item header-title" style={{color:'black'}}>LIKE.ME</p>
+          </div>
+        </div>        
+      </div>
 
-        {/* HEADER */}
-        <div className="hero-head" style={{margin:"0 auto"}}>
-          <div className="columns is-mobile is-marginless heading has-text-weight-bold">
-            <div className="column center">
-              <p className="navbar-item header-title" style={{color:'black'}}>LIKE.ME</p>
-            </div>
-          </div>        
-        </div>
+      <div className="hero-body">
 
-        <div className="hero-body">
+        <div style={{margin:'0 auto',marginBottom:'auto'}}>
 
-          <div style={{margin:'0 auto',marginBottom:'auto'}}>
+          <div className="columns is-mobile is-centered is-vcentered">
+            <div className="column is-two-fifths">
+              <span>
+      
+                <p className="title title-font-style hide-mobile" style={{color:'black'}}>Find people with similar music <br/> taste  as you on Spotify</p>
+                <a href={ SPOTIFY_AUTH_URL }>                 
+                  <button className="button is-black desktop-style sub-text">Find Matches</button>
+                </a>
 
-            <div className="columns is-mobile is-centered is-vcentered">
-              <div className="column is-two-fifths">
-                <span>
-        
-                  <p className="title title-font-style hide-mobile" style={{color:'black'}}>Find people with similar music <br/> taste  as you on Spotify</p>
-                  <a href={ SPOTIFY_AUTH_URL }>                 
-                    <button className="button is-black desktop-style sub-text">Find Matches</button>
-                  </a>
-
-                </span>
-              </div>
-
-              <div className="column">
-                <Image
-                src="/icons/icons8-spotify-500.png"
-                alt="Spotify Logo"
-                height='500px'
-                width='500px'
-                />  
-              </div>
-              
+              </span>
             </div>
 
-
-            {/* <button className="button is-black block desktop-style sub-text">Find Matches</button> */}
-
-            <div className="box has-text-centered" style={{marginTop:'2em'}}>
-
-              <p className="subtitle sub-text-font" style={{color:'black'}}>How It Works</p>
-
-              <div className="block" style={{marginRight:'5px',marginLeft:'5px'}}>
-                <p className="block sub-text">1. Connect to your Spotify account</p>
-                <p className="block sub-text">
-                  2. We generate matches based off who has the highest percentage of similar music as you based 
-                  off your liked songs
-                </p>
-                <p className="block sub-text">
-                  3. See a list of users ranked from highest matching percentage to least
-                </p>
-              </div>
-
+            <div className="column">
               <Image
-              src="/open-doodles-groovy.png"
-              alt="Groovy Guy Dancing"
-              height="200px"
-              width="200px"
-              />
+              src="/icons/icons8-spotify-500.png"
+              alt="Spotify Logo"
+              height='500px'
+              width='500px'
+              />  
+            </div>
+            
+          </div>
 
+
+          {/* <button className="button is-black block desktop-style sub-text">Find Matches</button> */}
+
+          <div className="box has-text-centered" style={{marginTop:'2em'}}>
+
+            <p className="subtitle sub-text-font" style={{color:'black'}}>How It Works</p>
+
+            <div className="block" style={{marginRight:'5px',marginLeft:'5px'}}>
+              <p className="block sub-text">1. Connect to your Spotify account</p>
+              <p className="block sub-text">
+                2. We generate matches based off who has the highest percentage of similar music as you based 
+                off your liked songs
+              </p>
+              <p className="block sub-text">
+                3. See a list of users ranked from highest matching percentage to least
+              </p>
             </div>
 
+            <Image
+            src="/open-doodles-groovy.png"
+            alt="Groovy Guy Dancing"
+            height="200px"
+            width="200px"
+            />
 
           </div>
-          
-
-    
-
-
-
-
 
 
         </div>
-
-
-
-
-
-
-      </section>
-
-
-    </body>
-
-
+      </div>
+    </section>
   </>   
   )
 }
