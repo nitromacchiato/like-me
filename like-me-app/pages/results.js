18dcf6dc44fe145c1ff88914fc2cb4c7c5b458c8
nitro <nitromacchitato@users.noreply.github.com>
2021-03-23 12:47:36 -0400

    updated some settings

--- like-me-app/pages/results.js
+++ like-me-app/pages/results.js
@@ -41,9 +41,10 @@ export default function Results({matches}) {
   return (
     <>
     <Head>
+      <Link href={process.env.NEXT_URL}>
       <title>Like.Me</title>
-      <link rel="icon" href="/favicon.ico" />
-      <link href="/fonts/style.css" rel="stylesheet"/>
+      </Link>
+      <link href="../fonts/style.css" rel="stylesheet"/>
     </Head>
 
 
@@ -93,54 +94,7 @@ export default function Results({matches}) {
             <div className="card-spacing">
               <ProfileCard displayName={matches[5][0]} profileImage={matches[5][2]} percentage={Math.round(matches[5][3])} profilePage={matches[5][1]}/>
             </div>
-            <div className="card-spacing">
-              <ProfileCard displayName={matches[6][0]} profileImage={matches[6][2]} percentage={Math.round(matches[6][3])} profilePage={matches[6][1]}/>
-            </div>
-            <div className="card-spacing">
-              <ProfileCard displayName={matches[7][0]} profileImage={matches[7][2]} percentage={Math.round(matches[7][3])} profilePage={matches[7][1]}/>
-            </div>
-            <div className="card-spacing">
-              <ProfileCard displayName={matches[8][0]} profileImage={matches[8][2]} percentage={Math.round(matches[8][3])} profilePage={matches[8][1]}/>
-            </div>
-            <div className="card-spacing">
-              <ProfileCard displayName={matches[8][0]} profileImage={matches[8][2]} percentage={Math.round(matches[8][3])} profilePage={matches[8][1]}/>
-            </div>
-            <div className="card-spacing">
-              <ProfileCard displayName={matches[9][0]} profileImage={matches[9][2]} percentage={Math.round(matches[9][3])} profilePage={matches[9][1]}/>
-            </div>
-            <div className="card-spacing">
-              <ProfileCard displayName={matches[10][0]} profileImage={matches[10][2]} percentage={Math.round(matches[10][3])} profilePage={matches[10][1]}/>
-            </div>
-            <div className="card-spacing">
-              <ProfileCard displayName={matches[11][0]} profileImage={matches[11][2]} percentage={Math.round(matches[11][3])} profilePage={matches[11][1]}/>
-            </div>
-            <div className="card-spacing">
-              <ProfileCard displayName={matches[12][0]} profileImage={matches[12][2]} percentage={Math.round(matches[12][3])} profilePage={matches[12][1]}/>
-            </div>
-            <div className="card-spacing">
-              <ProfileCard displayName={matches[13][0]} profileImage={matches[13][2]} percentage={Math.round(matches[13][3])} profilePage={matches[13][1]}/>
-            </div>
-            <div className="card-spacing">
-              <ProfileCard displayName={matches[14][0]} profileImage={matches[14][2]} percentage={Math.round(matches[14][3])} profilePage={matches[14][1]}/>
-            </div>
-            <div className="card-spacing">
-              <ProfileCard displayName={matches[15][0]} profileImage={matches[15][2]} percentage={Math.round(matches[15][3])} profilePage={matches[15][1]}/>
-            </div>
-            <div className="card-spacing">
-              <ProfileCard displayName={matches[16][0]} profileImage={matches[16][2]} percentage={Math.round(matches[16][3])} profilePage={matches[16][1]}/>
-            </div>
-            <div className="card-spacing">
-              <ProfileCard displayName={matches[17][0]} profileImage={matches[17][2]} percentage={Math.round(matches[17][3])} profilePage={matches[17][1]}/>
-            </div>
-            <div className="card-spacing">
-              <ProfileCard displayName={matches[18][0]} profileImage={matches[18][2]} percentage={Math.round(matches[18][3])} profilePage={matches[18][1]}/>
-            </div>
-            <div className="card-spacing">
-              <ProfileCard displayName={matches[19][0]} profileImage={matches[19][2]} percentage={Math.round(matches[19][3])} profilePage={matches[19][1]}/>
-            </div>
-            <div className="card-spacing">
-              <ProfileCard displayName={matches[20][0]} profileImage={matches[20][2]} percentage={Math.round(matches[20][3])} profilePage={matches[20][1]}/>
-            </div>
+
 
           </div>
 
