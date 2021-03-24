92d549441d4ecbc382f056cf30a144329b5b5340
nitro <nitromacchitato@users.noreply.github.com>
2021-03-24 19:55:02 -0400

    minor updates

--- like-me-app/pages/index.js
+++ like-me-app/pages/index.js
@@ -1,8 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
-import React, { useState } from "react"
-import { useRouter } from 'next/router'
+import React from "react"
 import 'bulma/css/bulma.css'
 
 
