457737811bb05f958945bb1c5f40281578cb7400
nitro <nitromacchitato@users.noreply.github.com>
2021-03-30 12:48:50 -0400

    minor bug fix where results dont display for users

--- like-me-app/pages/results.js
+++ like-me-app/pages/results.js
@@ -165,7 +165,7 @@ export default function Home({SpotifyAuthCode}) {
     setUserMatches(GeneratedMatches)
     
 
-   if(GeneratedMatches.length > 2){
+   if(GeneratedMatches.length > 0){
      setLoading(false)
    }
 
