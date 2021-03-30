93ea0d7bbabe8aee6ff6b51d2b493e4686949a16
nitromacchiato <nitromacchiato@gmail.com>
2021-03-30 00:38:30 -0400

    added backup user image if the user has no spotify default profile picture

--- like-me-app/components/card.js
+++ like-me-app/components/card.js
@@ -9,14 +9,25 @@ export default function ProfileCard({displayName,profileImage,percentage,profile
         <div className="columns is-multiline is-vcentered is-mobile is-11">
             <div className="column is-one-fifth  box has-text-centered button" style={{margin:"0 auto",width:'154px',height:'180px'}}>
                 <div className="has-text-centered ">
+                    {profileImage && 
+                        <a href={profilePage}  target="_blank">
+                            <div>
+                                <figure className="image image_placement">
+                                    <img style={{maxWidth:'auto',maxHeight:'170px'}} src={profileImage} />
+                                </figure>
+                            </div>
+                        </a>
+                    }
+
+                    {!profileImage && 
                     
-                    <a href={profilePage}  target="_blank">
-                        <div>
+                        <div style={{maxWidth:'auto',maxHeight:'170px'}}>
                             <figure className="image image_placement">
-                                <img style={{maxWidth:'auto',maxHeight:'170px'}} src={profileImage} />
+                                <img style={{maxWidth:'auto',maxHeight:'170px'}} src="/user.png" />
                             </figure>
                         </div>
-                    </a>
+
+                    }
                     
                     <div style={{marginTop:'1em'}}>
                         
