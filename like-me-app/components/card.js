3da185105adf18b20abb8df45ea62f16dfd8dda7
nitromacchiato <nitromacchiato@gmail.com>
2021-03-30 01:43:08 -0400

    added icon pictures and randomly assign them if the user has no profile picture

--- like-me-app/components/card.js
+++ like-me-app/components/card.js
@@ -6,28 +6,30 @@ import {useState} from "react"
 
 export default function ProfileCard({displayName,profileImage,percentage,profilePage}){
 
-    const [picture,setPicture] = useState("")
+    // Default pictures for users without a Spotify profile image 
+    const defaultPictures = ['/character_1.png','/character_2.png','/character_3.png','/character_4.png','/character_5.png','/character_6.png','/character_7.png','/character_8.png',,'/character_9.png','/character_10.png']
 
-    const https = profileImage.split(':')[0] || null 
 
-    if(https === "https"){
-        setPicture(profileImage)
-    } else {
-        setPicture("/user.png")
+    if(profileImage.length == 0 ){
+
+        // Get a random number from 0 - 10 
+        const randomNumber = Math.floor(Math.random() * 11); 
+        profileImage = defaultPictures[randomNumber]
+
+
     }
-
 
 
     return(
         
         <div className="columns is-multiline is-vcentered is-mobile is-11">
-            <div className="column is-one-fifth  box has-text-centered button" style={{margin:"0 auto",width:'154px',height:'180px'}}>
+            <div className="column is-one-fifth  box has-text-centered button" style={{margin:"0 auto",width:'154px',height:'auto'}}>
                 <div className="has-text-centered ">
                 
                     <a href={profilePage}  target="_blank">
                         <div>
-                            <figure className="image image_placement">
-                                <img style={{maxWidth:'auto',maxHeight:'170px'}} src={picture} />
+                            <figure className="image is-128x128 image_placement">
+                                <img src={profileImage} />
                             </figure>
                         </div>
                     </a>
@@ -36,8 +38,8 @@ export default function ProfileCard({displayName,profileImage,percentage,profile
                     
                     <div style={{marginTop:'1em'}}>
                         
-                        <p className="has-text-weight-light playlist_subtitles">{displayName}</p>
-                        <p className="has-text-weight-light playlist_subtitles percentage">{percentage}%</p>
+                        <p className="has-text-weight-normal playlist_subtitles">{displayName}</p>
+                        <p className="has-text-weight-bold playlist_subtitles percentage">{percentage}%</p>
                         
                     </div>
 
--- like-me-app/pages/results.js
+++ like-me-app/pages/results.js
@@ -122,7 +122,11 @@ function Matches(Results){
             
             </div>
         </div>
+    
     </div>
+
+    
+    <div className="has-text-centered">Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
   </section>
 
 
